8048b29b6c7e9aacd450d6cc041144f8cf5eed56
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-07 14:53:34 -0500

    Completed Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempOld) {
+  const tempNew = (tempOld - 32) * 5 / 9;
+  return (tempNew % 1 )? Math.round(tempNew *10) / 10 : tempNew;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempOld) {
+  const tempNew = (tempOld * 9 / 5) + 32;
+  return (tempNew % 1 )? Math.round(tempNew *10) / 10 : tempNew;
 };
 
 // Do not edit below this line
