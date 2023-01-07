1393e2975d969baa28bf40da3d1343833fcf17d6
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-07 03:09:21 -0500

    Completed Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    if (intA < 0 || intB < 0 || typeof intA !== 'number' || typeof intB !== 'number') return 'ERROR';
+    let larger;
+    let smaller;
+    let sum = 0;
+    if (intA > intB) {
+        larger = intA;
+        smaller = intB;
+    }
+    else if (intA <= intB) {
+        larger = intB;
+        smaller = intA;
+    }
+    while (smaller <= larger) {
+        sum += smaller++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
