e39c9ba92e57dc8a6a7fb277ca8102ac9e893124
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-07 01:22:41 -0500

    Completed Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverse = [];
+    for (i = 0; i < string.length; i++) {
+        reverse.unshift(string[i]);
+    }
+    return reverse.join('');
 };
 
 // Do not edit below this line
