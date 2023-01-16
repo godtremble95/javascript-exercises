08e030aadbe216c1894a5edfe27487462ccd62be
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-15 23:57:13 -0500

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let temp, numHigh = 1, numLow = 0;
+  if (num < 0) return "OOPS";
+  for (i = Number(num) -1; i > 0; i--) {
+    temp = numHigh + numLow;
+    numLow = numHigh;
+    numHigh = temp;
+  }
+  return numHigh;
 };
 
 // Do not edit below this line
