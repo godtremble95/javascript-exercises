436a36e2c5e3add8bff027c884a8f395da8bb773
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-16 00:57:16 -0500

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.sort((personA, personB) => {
+    return ((deadOrAlive(personB) - personB.yearOfBirth) -
+      (deadOrAlive(personA) - personA.yearOfBirth));
+  });
+  return people[0];
 };
+
+const deadOrAlive = (person) => ('yearOfDeath' in person) ?
+  person.yearOfDeath :
+  new Date().getFullYear();
 
 // Do not edit below this line
 module.exports = findTheOldest;
