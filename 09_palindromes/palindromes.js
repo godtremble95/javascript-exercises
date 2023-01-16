7fe4e4a9902ead1d699346088a46c5fc91406b69
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-15 23:39:01 -0500

    Completed Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (testStr) {
+  const charRemove = [' ', ',', '.', '!']
+  let testArr = testStr.toLowerCase().split('');
+  testArr = testArr.filter((char) => (!charRemove.some((punct) => char === punct)));
+  testStr = testArr.toString();
+  //return testArr === testArr.reverse();
+  return testStr === testArr.reverse().toString();
 };
 
 // Do not edit below this line
